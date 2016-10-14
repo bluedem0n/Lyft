3b2c88f05d6d12b0e41680a49598edea5dea549c
Sthefany <Sthefany>
2016-10-13 19:18:18 -0500

    Verificando numeros y mejorando js

--- js/app.js
+++ js/app.js
@@ -5,36 +5,47 @@
 // - Generar un código aleatorio con la estructura LAB-XYZ
 // - Validar lo obvio
 
-$(document).ready(function () {
-	$("#numero").keydown(function (evento) {
-		var ascii = evento.keyCode;
-		if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
-			return true;
-		} else {
-			return false;
-		}
-	});
-	$("#numero").keyup(function (evento) {
-		var longitud = $(this).val().length;
-		if (longitud == 9) {
-			$("#siguiente").attr("href", "signup.html");
-		} else {
-			$("#siguiente").removeAttr("href");
-		}
-	});
-	$("#numero").keypress(function (evento) {
-		var longitud = $(this).val().length;
-		if (longitud < 9) {
-			return true;
-		} else {
-			return false;
-		}
-	});
-	$("#siguiente").click(function (evento) {
-		var longitud = $("#numero").val().length;
-		evento.preventDefault();
-		if(longitud === 9){
-				var code = "LAB-";
+var cargar = function () {
+	$("#numero").keydown(numbersValidation);
+	$("#numero").keyup(longNumbers);
+	$("#numero").keypress(keyDisable);
+	$("#siguiente").click(codeGenerator);
+	$(".codigo").keypress(longCode);
+	$(".codigo").keydown(numbersValidation);
+}
+
+$(document).ready(cargar);
+
+function numbersValidation(e) {
+	var ascii = e.keyCode;
+	if (ascii == 8 || ascii == 9 || (ascii >= 48 && ascii <= 57)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function longNumbers(e) {
+	if ($(this).val().length == 9) {
+		$("#siguiente").attr("href", "verifynumber.html");
+	} else {
+		$("#siguiente").removeAttr("href");
+	}
+}
+
+function keyDisable(e) {
+	if ($(this).val().length < 9) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function codeGenerator(e) {
+	var longitud = $("#numero").val().length;
+	e.preventDefault();
+	if (longitud === 9) {
+		var code = "LAB-";
 		var aleatorio = Math.floor((Math.random() * 900) + 99);
 		swal({
 			title: "Tu codigo aleatorio es : ",
@@ -46,6 +57,13 @@ $(document).ready(function () {
 		}, function () {
 			window.location.href = $("#siguiente").attr("href");
 		});
-		}
-	});
-});
+	}
+}
+
+function longCode(e) {
+	if ($(this).val().length === 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
